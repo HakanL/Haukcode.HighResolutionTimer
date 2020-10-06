958bb3a5ed9d9ae8d211430f93af1f0c06568aaa
Hakan Lindestaf <hakan@lindestaf.com>
2020-10-05 23:32:19 -0500

    Initial commit

---
+++ src/ITimer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Haukcode.HighResolutionTimer
+{
+    public interface ITimer : IDisposable
+    {
+        void SetPeriod(int periodMS);
+
+        void WaitForTrigger();
+
+        void Start();
+
+        void Stop();
+    }
+}
