823067523f0be0d9428da063835ff5d61afeb9f2
Timur Vafin <51379496+redplcs@users.noreply.github.com>
2026-05-26 20:01:08 +0500

    Removed code duplication and simplified timer implementation.

---
+++ src/Implementations/ITimerImplementation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading;
+
+namespace Haukcode.HighResolutionTimer.Implementations
+{
+    internal interface ITimerImplementation : IDisposable
+    {
+        TimeSpan Period { get; set; }
+
+        void Wait(CancellationToken cancellationToken);
+    }
+}
